4254eec75c284cfad254a3974d530e7279182ab1
Simerjit Kaur <simerjitparmar@gmail.com>
2019-11-17 13:06:58 -0500

    web client models

---
+++ ClientAppMovieReview/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieReviewAPI.Models
+{
+    public partial class Movie
+    {
+        public Movie()
+        {
+            MovieComment = new HashSet<MovieComment>();
+            MovieRating = new HashSet<MovieRating>();
+        }
+
+        public int MovieId { get; set; }
+        public string MovieTitle { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateReleased { get; set; }
+        public string Genre { get; set; }
+        public string Actor { get; set; }
+        public string MovieImage { get; set; }
+        public string MovieVideo { get; set; }
+
+        public virtual ICollection<MovieComment> MovieComment { get; set; }
+        public virtual ICollection<MovieRating> MovieRating { get; set; }
+    }
+}
